4f0e5e2a9cdeb784e9eebd7047e8a01f0de181f2
Marcela Lasso <edith_lasso@pmi.org>
2023-11-17 18:10:33 -0500

    Add project files.

---
+++ MyExampleTests1/PasteTests.cs
@@ -0,0 +1,49 @@
+﻿using EllipticCurve.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyExample;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+
+namespace Paste.Tests
+{
+    [TestClass]
+    public class PasteTests
+    {
+        public FirefoxDriver Driver { get; set; }
+        public WebDriverWait Wait { get; set; }
+        [TestInitialize]
+        public void SetupTest()
+        {
+            var firefoxOptions = new FirefoxOptions();
+            firefoxOptions.AcceptInsecureCertificates = true;
+            Driver = new FirefoxDriver(firefoxOptions);
+            Wait = new WebDriverWait(this.Driver, TimeSpan.FromSeconds(30));  
+        }
+        [TestCleanup]
+        public void TeardownTest()
+        {
+            Driver.Quit();
+        }
+        [TestMethod]
+        public void RightUser_First()
+        {
+            PastePage searchEmailMainPage = new PastePage(Driver);
+            //searchEmailMainPage.Navigate();
+            Driver.Url = "https://pastebin.com";
+            searchEmailMainPage.Login();
+            searchEmailMainPage.Search("TestML", "Password123.");
+            PastePage gmailPage = new PastePage(Driver);
+            //gmailPage.ValidateResultsCount("Google Account: test1");
+        }
+    }
+}
---
+++ MyTEST/LoginPaste.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.PageObjects;
+using System.Threading;
+
+namespace MyExample
+{
+    public class PastePage
+    {
+        private readonly IWebDriver driver;
+        private readonly string url = "https://pastebin.com";
+        
+        public PastePage(IWebDriver browser)
+        {
+            this.driver = browser;
+        //    PageFactory.InitElements(browser, this);
+        }
+        [FindsBy(How = How.Id, Using = "loginform-username")]
+        public IWebElement UnameBox { get; set; }
+        
+        [FindsBy(How = How.XPath, Using = "//button[contains(text(),'Login')]")]
+        public IWebElement GoButton { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "loginform-password")]
+        public IWebElement PasswordBox { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//div[@class='OyEIQ uSvLId']//div[@jsname='B34EJ']")]
+        public IWebElement ValidationLabel { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//a[@class='btn-sign sign-in']")]
+        private readonly By buttontest = By.XPath("//a[@class='btn-sign sign-in']");
+        public IWebElement LoginButton { get; set; }
+        public void Navigate()
+        {
+            this.driver.Navigate().GoToUrl(this.url);
+        }
+        public void Login()
+        {
+            Thread.Sleep(6000);
+            driver.FindElement(buttontest).Click();
+            Thread.Sleep(6000);
+        }
+        public void Search(string uname, string pwd)
+        {
+            UnameBox.Clear();
+            UnameBox.SendKeys(uname);
+            PasswordBox.SendKeys(pwd);
+            GoButton.Click();
+        }
+
+        public void ValidateMessage(string expectedText)
+        {
+            Assert.IsTrue(this.ValidationLabel.Text.Contains(expectedText));
+        }
+    }
+}
